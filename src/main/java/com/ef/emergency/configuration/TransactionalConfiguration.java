1db03b216625c755a7843ecc480829ee0a007ef2
Michal Keder <keder@cngroup.dk>
2024-07-11 13:40:19 +0200

    application upload

---
+++ src/main/java/com/ef/emergency/configuration/TransactionalConfiguration.java
@@ -0,0 +1,9 @@
+package com.ef.emergency.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionalConfiguration {
+}
